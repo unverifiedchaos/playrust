07339b1b7b7d8377684eb35746ef26b474fc21e7
unverifiedchaos <saifullahkhan@cmr.ac.in>
2021-12-15 00:04:33 +0530

    fixed api

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use reqwest::Error;
+use std::env;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -11,10 +12,25 @@ struct Coffee {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let req_url = format!("https://api.sampleapis.com/coffee/hot");
-    println!("{}", req_url);//converts string to input
-    let response = reqwest::get(&req_url).await?;
-    let drinks: Vec<Coffee> = response.json().await?;
-    println!("{:?}", drinks[2].title);
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        let req_url = format!("https://api.sampleapis.com/coffee/hot");
+        println!("{}", req_url);//converts string to input
+        let response = reqwest::get(&req_url).await?;
+        let drinks: Vec<Coffee> = response.json().await?;
+        println!("{:?}", drinks);
+
+    } else {
+
+        let result: usize = args[1].parse().unwrap();
+        println!("{:?}", result);
+        let req_url = format!("https://api.sampleapis.com/coffee/hot");
+        let response = reqwest::get(&req_url).await?;
+        let drinks: Vec<Coffee> = response.json().await?;
+        println!("Title: {:?}\n", drinks[result].title);
+        println!("Description: {:?}\n", drinks[result].description);
+        println!("Ingredients: {:?}\n", drinks[result].ingredients);
+    }
     Ok(())
 }
+
